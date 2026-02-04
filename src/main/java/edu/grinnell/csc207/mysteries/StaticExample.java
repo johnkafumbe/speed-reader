f9f976d1420fa6da0ca2111d8d1569762ed59dd5
littleca <carrieannelittle61@gmail.com>
2026-02-04 11:57:15 -0600

    Progress was made -- left off 1.3

--- src/main/java/edu/grinnell/csc207/mysteries/StaticExample.java
+++ src/main/java/edu/grinnell/csc207/mysteries/StaticExample.java
@@ -6,17 +6,21 @@ class StaticCounterExample {
         value = 0;
     }
     public void increment(int value) {
-        this.value += value;
+        StaticCounterExample.value += value;
     }
 }
 
 public class StaticExample {
-    public void printGreeting() {
+    public static void printGreeting() {
         System.out.println("Hello World!");
     }
 
     public static void main(String[] args) {
         // Uncomment this line and find out if it works!
-        // printGreeting();
+        printGreeting();
+        StaticCounterExample ex1 = new StaticCounterExample();
+        StaticCounterExample ex2 = new StaticCounterExample();
+        ex1.increment(3);
+        System.out.println("ex1=" + ex1.value + " ex2 = " + ex2.value); 
     }
 }
