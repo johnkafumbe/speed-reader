565b4157aa6d4c108f7b3a12cace3f8f1bdc1b68
littleca <carrieannelittle61@gmail.com>
2026-02-08 17:59:18 -0600

    SpeedReader main started

--- src/main/java/edu/grinnell/csc207/speedreader/SpeedReader.java
+++ src/main/java/edu/grinnell/csc207/speedreader/SpeedReader.java
@@ -9,8 +9,32 @@ public class SpeedReader {
 
     /** TODO: implement me and add an appropriate doc comment! */
 
+    public int sleepTime(int wpm){
+        int time = 60000 / wpm;
+        return time;
+    } 
+
+    public void
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        if (args.length < 5){
+            System.out.println("Incorrect argurments. Please print filename, width, height, font size and words per minute");
+            return;
+        }
+        String filename = args[0];
+        int w = Integer.parseInt(args[1]);
+        int h = Integer.parseInt(args[2]);
+        int fontSz = Integer.parseInt(args[3]);
+        int wpm = Integer.parseInt(args[4]);
+
+        WordGenerator excerpt = new WordGenerator(filename);
+        DrawingPanel panel = new DrawingPanel(w, h);
+        Font f = new Font("Courier", Font.BOLD, fontSz);
+
+        
+
+
     }  
 }
