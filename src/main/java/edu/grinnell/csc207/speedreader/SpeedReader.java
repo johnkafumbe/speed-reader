9566a6b19b8c00ee155f461b5e4b7bff3b4fbd20
johnkafumbe <kafumbejbrown@gmail.com>
2026-02-09 12:31:27 -0600

    Speed Reader John's update with functional code and documentation with 4 text files

--- src/main/java/edu/grinnell/csc207/speedreader/SpeedReader.java
+++ src/main/java/edu/grinnell/csc207/speedreader/SpeedReader.java
@@ -3,38 +3,62 @@ package edu.grinnell.csc207.speedreader;
 import java.awt.*;
 import java.io.IOException;
 
-/** TODO: implement this class and add a doc comment! */
+/** 
+ * This is the main program that runs the speed reader. 
+ * It takes a file and flashes the words one by one on a screen.
+*/
 
-public class SpeedReader {
+public class SpeedReader{
 
-    /** TODO: implement me and add an appropriate doc comment! */
+    /** 
+     * The main method that starts the program.
+     * @param args are filename, width, height, font size, and WPM.
+    */
+   
+    public static void main(String[] args) throws Exception {
 
-    public int sleepTime(int wpm){
-        int time = 60000 / wpm;
-        return time;
-    } 
-
-    public void
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        if (args.length < 5){
+        if (args.length != 5){
             System.out.println("Incorrect argurments. Please print filename, width, height, font size and words per minute");
             return;
         }
+        
+        //Define Arguments
         String filename = args[0];
         int w = Integer.parseInt(args[1]);
         int h = Integer.parseInt(args[2]);
         int fontSz = Integer.parseInt(args[3]);
         int wpm = Integer.parseInt(args[4]);
 
+        int sleepTime = 60000 / wpm;
+
+        // Using word generator,drawing panel and graphics tools.
         WordGenerator excerpt = new WordGenerator(filename);
         DrawingPanel panel = new DrawingPanel(w, h);
+        Graphics g = panel.getGraphics();
         Font f = new Font("Courier", Font.BOLD, fontSz);
+        g.setFont(f);
 
-        
 
+        //loop for displaying words on the panel
+        while (excerpt.hasNext()) {
+
+            // Fill the rectangle with a white screen before the word is displayed
+            g.setColor(Color.WHITE);
+            g.fillRect(0, 0, w, h);
+            
+            // Set the color of the word to black and pull the word from the word generator, then display
+            // the word using drawstring to display on the panel.
+            g.setColor(Color.BLACK);
+            String word = excerpt.next();
+            g.drawString(word, w / 4, h / 4);
+            Thread.sleep(sleepTime);
+        }
+
+        // When the loop is done display the word and sentence count.
+        System.out.println("Total words: " + excerpt.getWordCount());
+        System.out.println("Total sentences: " + excerpt.getSentenceCount());
+            
+        }
 
     }  
-}
+
--- src/main/java/edu/grinnell/csc207/speedreader/WordGenerator.java
+++ src/main/java/edu/grinnell/csc207/speedreader/WordGenerator.java
@@ -5,26 +5,39 @@ import java.io.File;
 import java.io.IOException;
 
 
-/** TODO: implement this class and add a doc comment! */
+/** 
+ * This class reads a text file word by word and keeps track
+ * of how many words and sentences it has seen.
+*/
 public class WordGenerator {
     private Scanner scanner;
     private int wordCount;
     private int sentenceCount;
 
-    /** TODO: implement me and add an appropriate doc comment! */
+    /** 
+     * Opens the file and starts the word and sentence counters at zero.
+     * @param filename
+     */
     public WordGenerator(String filename) throws IOException {
         this.scanner = new Scanner (new File(filename));
         this.wordCount = 0;
         this.sentenceCount = 0;
     }
 
-    /** TODO: implement me and add an appropriate doc comment! */
+    /**
+     * Checks if there is another word left in the file to read.
+     * @return true if there is a word, false if the file is finished.
+     */
     public boolean hasNext() {
         return this.scanner.hasNext(); 
         
     }
 
-    /** TODO: implement me and add an appropriate doc comment! */
+    /** 
+     * Grabs the next word from the file and updates the counters.
+     * Checks the if the picked word ends with a punctuation that would make it a sentence.
+     * @return The next word as a String.
+     */
     public String next() {
         String word = this.scanner.next();
         this.wordCount++;
@@ -35,12 +48,16 @@ public class WordGenerator {
         return word;
     }
 
-    /** TODO: implement me and add an appropriate doc comment! */
+    /** 
+     * returns the number of words read so far.
+    */
     public int getWordCount() {
         return this.wordCount;
     }
 
-    /** TODO: implement me and add an appropriate doc comment! */
+    /** 
+     * Returns the number of sentences read so far.
+    */
     public int getSentenceCount() {
         return this.sentenceCount;
     }
